2cfe50e8400c7c7f66e20bbc4b06864d8330ef3d
ladokunAbisola <95176943+ladokunAbisola@users.noreply.github.com>
2022-03-01 04:00:02 +0100

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,9 +1,8 @@
-create database food; 
+CREATE database food; 
 CREATE TABLE food_detail (
 food_id int not null,
 food_name varchar (50) ,
 tag varchar (10) ,
 descriptions varchar (500),
 calories varchar (100),
-quantity varchar (100)
-);
+quantity varchar (100);
